51fe725fbaeb7f6ed426d19645228e310cd33c3d
matt.fleming <matt.fleming@polk-fl.net>
2016-07-06 12:09:41 -0400

    Quick write of ACA Error diagnosing program.

---
+++ ACA_XML_Reader/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ACA_XML_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_Check_File = new System.Windows.Forms.Button();
+            this.listBox_errorIDs = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_ErrorFile = new System.Windows.Forms.TextBox();
+            this.textBox_SubmissionFile = new System.Windows.Forms.TextBox();
+            this.recordID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SSN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Check_File
+            // 
+            this.button_Check_File.Location = new System.Drawing.Point(376, 439);
+            this.button_Check_File.Name = "button_Check_File";
+            this.button_Check_File.Size = new System.Drawing.Size(75, 23);
+            this.button_Check_File.TabIndex = 0;
+            this.button_Check_File.Text = "Check File";
+            this.button_Check_File.UseVisualStyleBackColor = true;
+            this.button_Check_File.Click += new System.EventHandler(this.button_Check_File_Click);
+            // 
+            // listBox_errorIDs
+            // 
+            this.listBox_errorIDs.FormattingEnabled = true;
+            this.listBox_errorIDs.Location = new System.Drawing.Point(12, 12);
+            this.listBox_errorIDs.Name = "listBox_errorIDs";
+            this.listBox_errorIDs.Size = new System.Drawing.Size(120, 95);
+            this.listBox_errorIDs.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.recordID,
+            this.firstName,
+            this.lastName,
+            this.SSN});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(771, 320);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // textBox_ErrorFile
+            // 
+            this.textBox_ErrorFile.Location = new System.Drawing.Point(430, 21);
+            this.textBox_ErrorFile.Name = "textBox_ErrorFile";
+            this.textBox_ErrorFile.Size = new System.Drawing.Size(266, 20);
+            this.textBox_ErrorFile.TabIndex = 5;
+            // 
+            // textBox_SubmissionFile
+            // 
+            this.textBox_SubmissionFile.Location = new System.Drawing.Point(430, 47);
+            this.textBox_SubmissionFile.Name = "textBox_SubmissionFile";
+            this.textBox_SubmissionFile.Size = new System.Drawing.Size(266, 20);
+            this.textBox_SubmissionFile.TabIndex = 6;
+            // 
+            // recordID
+            // 
+            this.recordID.HeaderText = "Record ID";
+            this.recordID.Name = "recordID";
+            // 
+            // firstName
+            // 
+            this.firstName.HeaderText = "First Name";
+            this.firstName.Name = "firstName";
+            // 
+            // lastName
+            // 
+            this.lastName.HeaderText = "Last Name";
+            this.lastName.Name = "lastName";
+            // 
+            // SSN
+            // 
+            this.SSN.HeaderText = "SSN";
+            this.SSN.Name = "SSN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 474);
+            this.Controls.Add(this.textBox_SubmissionFile);
+            this.Controls.Add(this.textBox_ErrorFile);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox_errorIDs);
+            this.Controls.Add(this.button_Check_File);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ACA Error Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Check_File;
+        private System.Windows.Forms.ListBox listBox_errorIDs;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_ErrorFile;
+        private System.Windows.Forms.TextBox textBox_SubmissionFile;
+        private System.Windows.Forms.DataGridViewTextBoxColumn recordID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SSN;
+    }
+}
+
---
+++ ACA_XML_Reader/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ACA_XML_Reader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        //Error File Read considered working.
+        private void readErrorFile() {
+            //Load Error file
+            XmlTextReader xmlTextRead_ErrorFile = new XmlTextReader(textBox_ErrorFile.Text);
+            Boolean ErrorRecordID = false;
+            while (xmlTextRead_ErrorFile.Read()) {
+                //Record ID.
+                if (xmlTextRead_ErrorFile.NodeType == XmlNodeType.Element) {
+                    if (xmlTextRead_ErrorFile.Name.ToString() == "UniqueRecordId") {
+                        ErrorRecordID = true;
+                    }
+                }
+                if (xmlTextRead_ErrorFile.NodeType == XmlNodeType.Text && ErrorRecordID) {
+                    string uniqueRecordID = xmlTextRead_ErrorFile.Value.ToString().Substring((xmlTextRead_ErrorFile.Value.ToString().IndexOf("|") + 1));
+                    uniqueRecordID = uniqueRecordID.Substring(uniqueRecordID.IndexOf("|") + 1);
+                    listBox_errorIDs.Items.Add(uniqueRecordID);//"Record ID: " + 
+                    ErrorRecordID = false;
+                }
+            }
+            //Close the readError file.
+            xmlTextRead_ErrorFile.Close();
+        }
+
+        private void readSubmissionFile() {
+            //Load submission file.
+            XmlTextReader xmlTextRead_SubmissionFile = new XmlTextReader(textBox_SubmissionFile.Text);
+
+            //XML Node Type
+            XmlNodeType nodeType;
+
+            //Status Variables.
+            Boolean isRecordID = false,
+                //Element field Type.
+                isFirstName = false,
+                isLastName = false,
+                isSSN = false,
+                badRecord = false;
+
+            string recordID = "";
+
+            string persFirstName = "",
+                persLastName = "",
+                persSSN = "",
+                previousPersSSN = "";
+
+            //Read IRS File
+            while (xmlTextRead_SubmissionFile.Read()) {
+                //Read the node type.
+                nodeType = xmlTextRead_SubmissionFile.NodeType;
+
+                //Information inside field
+                if (nodeType == XmlNodeType.Text) {
+                    //Get the current Value.
+                    string elementValueText = xmlTextRead_SubmissionFile.Value;
+
+                    if (isRecordID) {
+                        if (elementValueText != "") {
+                            foreach (string rowItem in listBox_errorIDs.Items) {
+                                if (rowItem == xmlTextRead_SubmissionFile.Value) {
+                                    recordID = rowItem;
+                                    badRecord = true;
+                                    break;
+                                }
+                            }
+                        }
+                        isRecordID = false;
+                    }
+
+                    if (badRecord) {
+                        if (isFirstName) {
+                            persFirstName = elementValueText;
+                            isFirstName = false;
+                        }
+                        else if (isLastName) {
+                            persLastName = elementValueText;
+                            isLastName = false;
+                        }
+                        else if (isSSN) { //previousPersSSN != elementValueText && elementValueText.Length == 9
+                            previousPersSSN = elementValueText;
+                            persSSN = elementValueText;
+                            isSSN = false;
+                        }
+                    }
+                }
+                else if (nodeType == XmlNodeType.Element) {
+                    string elementNameText = xmlTextRead_SubmissionFile.Name;
+                    /*
+                     * Record ID is first row of a new employee and dependent(s).
+                     * Only occurs once per set of employee and dependent(s)
+                     */
+                    if (elementNameText == "RecordId") {
+                        isRecordID = true;
+                        persFirstName = "";
+                        persLastName = "";
+                        persSSN = "";
+                        badRecord = false;
+                    }
+                    if (badRecord) {
+                        /*
+                         * First name should appear second in the XML.
+                         * After Record ID.
+                         * Can be non-existant 
+                         */
+                        if (elementNameText == "PersonFirstNm") {
+                            isFirstName = true;
+                        }
+                        /*
+                         * First name should appear third in the XML.
+                         * After First Name.
+                         * Can be non-existant 
+                         */
+                        if (elementNameText == "PersonLastNm") {
+                            isLastName = true;
+                        }
+                        /*
+                         * First name should appear forth in the XML.
+                         * After Last Name.
+                         * Social appears twice for employee once before the first name and another a few fields before that.
+                         * Can be non-existant for dependent.
+                         */
+                        if (elementNameText == "irs:SSN") {
+                            isSSN = true;
+                        }
+                        if (elementNameText == "CoveredIndividualGrp") {
+                            persFirstName = "{ERROR}";
+                            persLastName = "{ERROR}";
+                            persSSN = "{ERROR}";
+                        }
+                    }
+                }
+                else if (nodeType == XmlNodeType.EndElement) {
+                    string elementNameText = xmlTextRead_SubmissionFile.Name;
+                    if (elementNameText == "CoveredIndividualGrp") {
+                        if (persFirstName != "" || persLastName != "" || persSSN != "")
+                            //if (persFirstName != "{ERROR}" && persLastName != "{ERROR}" && persSSN != "{ERROR}")
+                                dataGridView1.Rows.Add(recordID, persFirstName, persLastName, persSSN);
+                    }
+                }
+            }
+            //File finished reading.
+            xmlTextRead_SubmissionFile.Close();
+        }
+
+        private void button_Check_File_Click(object sender, EventArgs e) {
+            readErrorFile();
+            readSubmissionFile();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            textBox_ErrorFile.Text = ofd.FileName;
+            ofd.ShowDialog();
+            textBox_SubmissionFile.Text = ofd.FileName;
+        }
+    }
+}
